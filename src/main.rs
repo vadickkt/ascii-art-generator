1b0f777f51009bd0344d48b2a80391964f9a7d59
vadickkt <vadwwxz@gmail.com>
2025-10-19 10:20:22 +0200

    get terminal width/height

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use image::{GenericImageView, ImageReader};
+use terminal_size::{terminal_size, Height, Width};
 
 fn main() {
-    let image = ImageReader::open("images/image_5x5.jpg")
+    let image = ImageReader::open("images/image_100x100.jpg")
         .expect("Image not found")
         .decode()
         .expect("Failed to decode image");
 
     let (width, height) = image.dimensions();
     println!("Image width: {}, height: {}", width, height);
+
+    let (terminal_width, terminal_height) = match terminal_size() {
+        Some((Width(w), Height(h))) => (w as usize, h as usize),
+        None => (80, 24), // default value
+    };
+    println!("Terminal width: {terminal_width}, height: {terminal_height}");
 
     let rgba_image = image.to_rgba8();
     let bytes = rgba_image.as_raw();
@@ -16,17 +23,25 @@ fn main() {
         let r = bytes[i] as f64;
         let g = bytes[i + 1] as f64;
         let b = bytes[i + 2] as f64;
-        let a = bytes[i + 3];
+        let a = bytes[i + 3] as f64 / 255.0; // normalize alpha
         let pixel_index = i / 4;
 
-        let brightness = (0.299 * r) + (0.587 * g) + (0.114 * g);
-        println!(
-            "Pixel {}: R={}, G={}, B={}, A={}, Brightness: {}" ,
-            pixel_index, r, g, b, a, brightness
-        );
-    }
-}
+        let r_blend = r * a;
+        let g_blend = g * a;
+        let b_blend = b * a;
 
+        let brightness = 0.299 * r_blend + 0.587 * g_blend + 0.114 * b_blend;
+        let char = brightness_to_char(brightness);
+
+        if pixel_index % width as usize == 0 {
+            println!();
+        }
+
+        print!("{} ", char);
+    }
+
+    println!();
+}
 fn brightness_to_char(brightness: f64) -> char {
     let ascii = ['@', '#', '%', '*', '+', '=', '-', ':', '.'];
     let index = ((brightness / 255.0) * (ascii.len() - 1) as f64).round() as usize;
