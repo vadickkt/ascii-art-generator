0cd8b7a4c1fd4345ad643cb730cebabecf39a7c8
vadickkt <vadwwxz@gmail.com>
2025-10-19 09:51:01 +0200

    printing all pixel in format (rgba) + calculating brightness for every pixel

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use image::{GenericImageView, ImageReader};
 
+fn main() {
+    let image = ImageReader::open("images/image_5x5.jpg")
+        .expect("Image not found")
+        .decode()
+        .expect("Failed to decode image");
+
+    let (width, height) = image.dimensions();
+    println!("Image width: {}, height: {}", width, height);
+
+    let rgba_image = image.to_rgba8();
+    let bytes = rgba_image.as_raw();
+
+    for i in (0..bytes.len()).step_by(4) {
+        let r = bytes[i] as f64;
+        let g = bytes[i + 1] as f64;
+        let b = bytes[i + 2] as f64;
+        let a = bytes[i + 3];
+        let pixel_index = i / 4;
+
+        let brightness = (0.299 * r) + (0.587 * g) + (0.114 * g);
+        println!(
+            "Pixel {}: R={}, G={}, B={}, A={}, Brightness: {}" ,
+            pixel_index, r, g, b, a, brightness
+        );
+    }
 }
